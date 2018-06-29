c10ea68ce519de704bca703a70bc5e79cc1a4be7
qiaochun ji <qiaochunji@jiqiaochun-macbook-pro.local>
2018-06-29 09:35:04 +0800

    Initial Commit

---
+++ NoteBook/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  NoteBook
+//
+//  Created by qiaochun ji on 2018/6/29.
+//  Copyright © 2018年 qiaochun ji. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ NoteBook/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  NoteBook
+//
+//  Created by qiaochun ji on 2018/6/29.
+//  Copyright © 2018年 qiaochun ji. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
